6558e89d4db44f71e3bd4226ed650f8b821f72cf
Marcin <marcin.miechowiak@gmail.com>
2024-10-03 21:01:57 +0200

    Added test - throw service exception upon starting match with incorrect names

--- src/test/java/com/scoreboard/service/FootballScoreServiceTest.java
+++ src/test/java/com/scoreboard/service/FootballScoreServiceTest.java
@@ -3,9 +3,12 @@ package com.scoreboard.service;
 import com.scoreboard.exception.ScoreServiceException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -44,5 +47,22 @@ public class FootballScoreServiceTest {
 
         // When & Then
         assertThrows(ScoreServiceException.class, () -> service.startMatch(homeTeam, awayTeam)); //2nd start try
+    }
+
+    @ParameterizedTest(name = "Throw ScoreServiceException when adding invalid team name match")
+    @MethodSource("invalidTeamNamesProvider")
+    public void shouldThrowExceptionWhenStartingMatchWithIncorrectNames(String homeTeam, String awayTeam) {
+        // When & Then:
+        assertThrows(ScoreServiceException.class, () -> service.startMatch(homeTeam, awayTeam));
+    }
+
+    private static Stream<Arguments> invalidTeamNamesProvider() {
+        return Stream.of(
+                Arguments.of("", "Team B"), // Empty home team
+                Arguments.of("Team A", ""), // Empty away team
+                Arguments.of(null, "Team A"), // Null home team
+                Arguments.of("Team A", null), // Null away team
+                Arguments.of("Team A", "Team A") // Same home and away team
+        );
     }
 }
