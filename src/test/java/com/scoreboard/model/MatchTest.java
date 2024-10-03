de9add27366597de8875ac9f0dcb6cadb79c6294
Marcin <marcin.miechowiak@gmail.com>
2024-10-03 19:42:33 +0200

    Added Test - create match scenario

---
+++ src/test/java/com/scoreboard/model/MatchTest.java
@@ -0,0 +1,32 @@
+package com.scoreboard.model;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class MatchTest {
+
+    @ParameterizedTest(name = "Create match between {0} and {1}")
+    @CsvSource({
+            "Team A, Team B",
+            "Team C, Team D",
+            "Team E, Team F"
+    })
+    public void shouldCreateMatch(String homeTeam, String awayTeam) {
+        // Given
+        Match match;
+
+        // When
+        match = new Match(homeTeam, awayTeam);
+
+        // Then
+        assertNotNull(match);
+        assertNotNull(match.getId());
+        assertEquals(homeTeam, match.getHomeTeam());
+        assertEquals(0, match.getHomeScore());
+        assertEquals(awayTeam, match.getAwayTeam());
+        assertEquals(0, match.getAwayScore());
+    }
+
+}
