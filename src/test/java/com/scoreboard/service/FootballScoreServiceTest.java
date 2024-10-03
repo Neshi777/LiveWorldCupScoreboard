a4b55f93e5b58025d56f60e11aee8aba9d72eff6
Marcin <marcin.miechowiak@gmail.com>
2024-10-03 20:42:58 +0200

    Added test - throw proper service exception upon adding existing match

--- src/test/java/com/scoreboard/service/FootballScoreServiceTest.java
+++ src/test/java/com/scoreboard/service/FootballScoreServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FootballScoreServiceTest {
     private FootballScoreService service;
@@ -31,5 +32,17 @@ public class FootballScoreServiceTest {
 
         // Then
         assertNotNull(matchId, "Match should be started - not null matchId");
+    }
+
+    @ParameterizedTest(name = "Throw ScoreServiceException when adding existing match")
+    @CsvSource({
+            "Mexico, Canada"
+    })
+    public void shouldThrowExceptionWhenStartingMatchExists(String homeTeam, String awayTeam) throws ScoreServiceException, InvalidTeamNameException {
+        // Given
+        service.startMatch(homeTeam, awayTeam); //match first start
+
+        // When & Then
+        assertThrows(ScoreServiceException.class, () -> service.startMatch(homeTeam, awayTeam));
     }
 }
